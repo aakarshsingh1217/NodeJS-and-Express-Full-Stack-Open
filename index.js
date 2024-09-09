ead6e326d6dacab444e8906400dc37b84e827330
Aakarsh Singh <aaks15721@gmail.com>
2024-09-09 12:06:46 +0930

    accidentally used comma instead of fullstop

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan');
 const cors = require('cors')
 const app = express()
-app,use(cors())
+app.use(cors())
 
 let persons = [
     { 
